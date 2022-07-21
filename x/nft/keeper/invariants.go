be9f4672796e276b926705086d47c41665c2f676
Isteb4k <rakitin.dmit@gmail.com>
2022-07-21 14:29:35 +0700

    improved proto files, improved stored in KVStore entities and relations

--- x/nft/keeper/invariants.go
+++ x/nft/keeper/invariants.go
@@ -4,8 +4,6 @@ package keeper
 
 import (
 	"bitbucket.org/decimalteam/go-smart-node/x/nft/types"
-	"fmt"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -24,36 +22,18 @@ func AllInvariants(k Keeper) sdk.Invariant {
 	}
 }
 
-// SupplyInvariant checks that the total amount of nfts on collections matches the total amount owned by addresses
+// SupplyInvariant checks that the total amount of nfts on collections matches the total amount nfts in store
 func SupplyInvariant(k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
-		collectionsSupply := make(map[string]int)
-		ownersCollectionsSupply := make(map[string]int)
-		var msg string
-		count := 0
-
+		totalSupply := 0
 		k.IterateCollections(ctx, func(collection types.Collection) bool {
-			collectionsSupply[collection.Denom] = collection.Supply()
+			totalSupply += collection.Supply()
 			return false
 		})
 
-		for _, owner := range k.GetOwners(ctx) {
-			for _, idCollection := range owner.IDCollections {
-				ownersCollectionsSupply[idCollection.Denom] += idCollection.Supply()
-			}
-		}
+		nfts := k.GetNFTs(ctx)
+		broken := len(nfts) != totalSupply
 
-		for denom, supply := range collectionsSupply {
-			if supply != ownersCollectionsSupply[denom] {
-				count++
-				msg += fmt.Sprintf("total %s NFTs supply invariance:\n"+
-					"\ttotal %s NFTs supply: %d\n"+
-					"\tsum of %s NFTs by owner: %d\n", denom, denom, supply, denom, ownersCollectionsSupply[denom])
-			}
-		}
-		broken := count != 0
-
-		return sdk.FormatInvariant(types.ModuleName, "supply", fmt.Sprintf(
-			"%d NFT supply invariants found\n%s", count, msg)), broken
+		return sdk.FormatInvariant(types.ModuleName, "supply", "NFT supply invariants found"), broken
 	}
 }
