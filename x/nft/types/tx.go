85be798c852d764ca948d41a754261da5c61e3d8
Vyacheslav Okulichev <khirn01@gmail.com>
2022-08-19 14:26:47 +0700

    feature/27533 fix: organize constants

--- x/nft/types/tx.go
+++ x/nft/types/tx.go
@@ -1,10 +1,10 @@
 package types
 
 import (
-	"bitbucket.org/decimalteam/go-smart-node/utils/helpers"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"regexp"
 	"strings"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 /* --------------------------------------------------------------------------- */
@@ -29,13 +29,6 @@ func NewMsgMintNFT(
 		AllowMint: allowMint,
 	}
 }
-
-const regName = "^[a-zA-Z0-9_-]{1,255}$"
-
-var MinReserve = sdk.NewInt(100)
-
-var NewMinReserve = helpers.BipToPip(sdk.NewInt(100))
-var NewMinReserve2 = helpers.BipToPip(sdk.NewInt(1))
 
 // Route Implements Msg
 func (m *MsgMintNFT) Route() string { return RouterKey }
