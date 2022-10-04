7430a6d984353dec0e9b212849813507a32bc921
Vyacheslav Okulichev <khirn01@gmail.com>
2022-10-04 08:34:10 +0700

    fix: bigfloat tests

--- types/bigfloat/log_test.go
+++ types/bigfloat/log_test.go
@@ -9,7 +9,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"bitbucket.org/decimalteam/go-smart-node/third_party/bigfloat"
+	"bitbucket.org/decimalteam/go-smart-node/types/bigfloat"
 )
 
 // See note in sqrt_test.go about which numbers
