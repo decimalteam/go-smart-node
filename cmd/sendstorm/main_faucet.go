7128d02607c5f065e8e335326af8956ce5e6d9fd
Vyacheslav Okulichev <khirn01@gmail.com>
2022-11-23 10:11:12 +0700

    hotfix: (sendstorm) external faucet address

--- cmd/sendstorm/main_faucet.go
+++ cmd/sendstorm/main_faucet.go
@@ -133,7 +133,7 @@ func cmdFaucetExt() *cobra.Command {
 			}
 
 			// do action
-			client := resty.New().SetHostURL("https://devnet-dec2.console.decimalchain.com/api/faucet")
+			client := resty.New().SetHostURL("https://devnet-gate.decimalchain.com/api/faucet")
 			for i, mnemonic := range mnemonics {
 				acc, err := dscWallet.NewAccountFromMnemonicWords(mnemonic, "")
 				if err != nil {
@@ -141,7 +141,7 @@ func cmdFaucetExt() *cobra.Command {
 					continue
 				}
 				fmt.Printf("account (%d) %s query\n", i, acc.Address())
-				resp, err := client.R().SetBody(map[string]string{"address": acc.Address()}).Post("https://devnet-dec2.console.decimalchain.com/api/faucet")
+				resp, err := client.R().SetBody(map[string]string{"address": acc.Address()}).Post("https://devnet-gate.decimalchain.com/api/faucet")
 				if err != nil {
 					fmt.Println(err)
 					continue
