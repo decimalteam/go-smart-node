67428d08264072f4a374b00de3aaba45cc3b8051
Vyacheslav Okulichev <khirn01@gmail.com>
2022-12-02 09:12:41 +0700

    hotfix: remove unnecessary EventReturnLegacyCoins

--- x/legacy/keeper/msg_server.go
+++ x/legacy/keeper/msg_server.go
@@ -4,13 +4,9 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/bech32"
 	"github.com/evmos/ethermint/crypto/ethsecp256k1"
 
-	"bitbucket.org/decimalteam/go-smart-node/cmd/config"
 	commonTypes "bitbucket.org/decimalteam/go-smart-node/types"
-	"bitbucket.org/decimalteam/go-smart-node/utils/events"
-	"bitbucket.org/decimalteam/go-smart-node/x/legacy/errors"
 	"bitbucket.org/decimalteam/go-smart-node/x/legacy/types"
 )
 
@@ -26,23 +22,14 @@ func (k Keeper) ReturnLegacy(goCtx context.Context, msg *types.MsgReturnLegacy) 
 	legacyAddress, _ := commonTypes.GetLegacyAddressFromPubKey(msg.PublicKey)
 	legacySdkAddress := sdk.MustAccAddressFromBech32(legacyAddress)
 	actualSdkAddress := sdk.AccAddress(ethsecp256k1.PubKey{Key: msg.PublicKey}.Address())
-	actualAddress, _ := bech32.ConvertAndEncode(config.Bech32Prefix, actualSdkAddress)
 
 	// send coins to new address if we have some balance on old address
 	coins := k.bankKeeper.GetAllBalances(ctx, legacySdkAddress)
 	if !coins.IsZero() {
+		// this send emit event 'transfer', which processed by worker
 		err := k.bankKeeper.SendCoins(ctx, legacySdkAddress, actualSdkAddress, coins)
 		if err != nil {
 			return nil, err
-		}
-		// Emit send event
-		err = events.EmitTypedEvent(ctx, &types.EventReturnLegacyCoins{
-			LegacyOwner: legacyAddress,
-			Owner:       actualAddress,
-			Coins:       coins,
-		})
-		if err != nil {
-			return nil, errors.Internal.Wrapf("err: %s", err.Error())
 		}
 	}
 
