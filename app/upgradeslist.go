3272a4feb0ac153f68d7e3782cbd5a88e63a9dd7
Vyacheslav Okulichev <khirn01@gmail.com>
2022-12-28 15:23:48 +0700

    fix: update block 12830301

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -2,5 +2,5 @@ package app
 
 // UpgradeList is application upgrade table. Different for different environments
 var UpgradeList = []UpgradeCreator{
-	{"https://repo.decimalchain.com/12828401", FixSendUpgradeHandlerCreator},
+	{"https://repo.decimalchain.com/12830301", FixSendUpgradeHandlerCreator},
 }
