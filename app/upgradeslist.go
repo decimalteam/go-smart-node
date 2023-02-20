cc97cc7d004414b0cce558f89458326316601a2b
Igor Sokolov <wardencliffe.sis@gmail.com>
2023-02-20 17:08:26 +0700

    feature/44640 fix: restoring testnet network after failed software upgrade occured because of upgrade handler was missed. [update]

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -3,4 +3,5 @@ package app
 // UpgradeList is application upgrade table. Different for different environments
 var UpgradeList = []UpgradeCreator{
 	{"https://testnet-repo.decimalchain.com/6489301", FixSendUpgradeHandlerCreator},
+	{"https://testnet-repo.decimalchain.com/7377901", DummyUpgradeHandlerCreator},
 }
