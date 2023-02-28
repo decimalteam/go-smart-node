46e5fd17553a4f0365da9c96fbcb0237ceae3338
hakerkiler <vinatorul2@gmail.com>
2023-02-28 10:15:10 +0300

    commit for test update devnet

--- app/ante/errors.go
+++ app/ante/errors.go
@@ -7,12 +7,12 @@ import (
 var codespace = "ante"
 
 var (
-	FeePayerAddressDoesNotExist    = errors.New(codespace, 1, "fee payer address does not exist")
+	FeePayerAddressDoesNotExist    = errors.New(codespace, 1, "fee payer address does not exist in network")
 	FeeLessThanCommission          = errors.New(codespace, 2, "insufficient funds to pay for fees")
 	FailedToSendCoins              = errors.New(codespace, 3, "failed to send coins")
 	InsufficientFundsToPayFee      = errors.New(codespace, 4, "insufficient funds to pay for fee")
 	InvalidFeeAmount               = errors.New(codespace, 5, "invalid fee amount")
-	UnknownTransaction             = errors.New(codespace, 6, "unknown transaction type 12")
+	UnknownTransaction             = errors.New(codespace, 6, "unknown transaction type")
 	CoinReserveInsufficient        = errors.New(codespace, 7, "coin reserve balance is not sufficient for transaction")
 	CoinReserveBecomeInsufficient  = errors.New(codespace, 8, "coin reserve will become lower than minimal reserve.")
 	NotFeeTxType                   = errors.New(codespace, 9, "x must be a FeeTx")
