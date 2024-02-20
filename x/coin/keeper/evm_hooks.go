63ae83c34e42efaa60752765e0f2980c74ea80e0
hakerkiler <vinatorul2@gmail.com>
2024-02-20 10:13:12 +0300

    [update] add function update VR of coin by event from evm

--- x/coin/keeper/evm_hooks.go
+++ x/coin/keeper/evm_hooks.go
@@ -9,7 +9,6 @@ import (
 	"bitbucket.org/decimalteam/go-smart-node/x/coin/types"
 	cointypes "bitbucket.org/decimalteam/go-smart-node/x/coin/types"
 	"cosmossdk.io/math"
-	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	evmtypes "github.com/decimalteam/ethermint/x/evm/types"
@@ -66,30 +65,40 @@ func (k Keeper) PostTxProcessing(
 	//}
 
 	coinCenter, _ := contracts.TokenCenterMetaData.GetAbi()
+	coinContract, _ := contracts.TokenMetaData.GetAbi()
 
 	methodId, err := coinCenter.MethodById(msg.Data)
 	if err != nil {
 		return nil
 	}
-	fmt.Print(methodId)
+
+	// this var is only for new token create from token center
+	var tokenAddress contracts.TokenCenterDeployed
+
+	for _, log := range recipient.Logs {
+		eventCenterByID, errEvent := coinCenter.EventByID(log.Topics[0])
+		if errEvent == nil {
+			if eventCenterByID.Name == "TokenDeployed" {
+				_ = coinCenter.UnpackIntoInterface(&tokenAddress, eventCenterByID.Name, log.Data)
+			}
+		}
+		eventCoinByID, errEvent := coinContract.EventByID(log.Topics[0])
+		if errEvent == nil {
+			if eventCoinByID.Name == "TokenReserveUpdated" {
+				var tokenUpdata contracts.TokenReserveUpdated
+				_ = coinCenter.UnpackIntoInterface(&tokenUpdata, eventCoinByID.Name, log.Data)
+
+			}
+		}
+	}
 
 	// Check if processed method
 	switch methodId.Name {
 	case types.DRC20MethodCreateToken:
 
-		var tokenAddress contracts.TokenCenterDeployed
-		for _, log := range recipient.Logs {
-			eventByID, errEvent := coinCenter.EventByID(log.Topics[0])
-			if errEvent == nil {
-				if eventByID.Name == "TokenDeployed" {
-					_ = coinCenter.UnpackIntoInterface(&tokenAddress, eventByID.Name, log.Data)
-				}
-			}
-		}
-
 		var tokenNew NewToken
 		err = contracts.UnpackInputsData(&tokenNew, methodId.Inputs, msg.Data[4:])
-		fmt.Print(tokenNew)
+
 		err = k.CreateCoinEvent(ctx, msg.Value, tokenNew.TokenData, tokenAddress.TokenAddress.String())
 		if err != nil {
 			return status.Error(codes.Internal, err.Error())
@@ -98,126 +107,34 @@ func (k Keeper) PostTxProcessing(
 		return nil
 	}
 
-	//for i, log := range receipt.Logs {
-	//	// Note: the `Transfer` event contains 3 topics (id, from, to)
-	//	if len(log.Topics) != 3 {
-	//		continue
-	//	}
-	//
-	//	// Check if event is included in ERC20
-	//	eventID := log.Topics[0]
-	//	methodId, err := coinCenter.MethodById(eventID)
-	//	if err != nil {
-	//		continue
-	//	}
-	//	// Check if event .
-	//	switch event.Name {
-	//	case types.DRC20EventCreateToken:
-	//		_, err = k.CallEVM(ctx, erc20, types.ModuleAddress, contractAddr, true, "burn", tokens)
-	//	case types.OWNER_EXTERNAL:
-	//		err = k.bankKeeper.MintCoins(ctx, types.ModuleName, coins)
-	//	default:
-	//		continue
-	//	}
-	//
-	//	// Check if event is a `Transfer` event.
-	//	if event.Name != types.ERC20EventTransfer {
-	//		k.Logger(ctx).Info("emitted event", "name", event.Name, "signature", event.Sig)
-	//		continue
-	//	}
-	//
-	//	transferEvent, err := erc20.Unpack(event.Name, log.Data)
-	//	if err != nil {
-	//		k.Logger(ctx).Error("failed to unpack transfer event", "error", err.Error())
-	//		continue
-	//	}
-	//
-	//	if len(transferEvent) == 0 {
-	//		continue
-	//	}
-	//
-	//	tokens, ok := transferEvent[0].(*big.Int)
-	//	// safety check and ignore if amount not positive
-	//	if !ok || tokens == nil || tokens.Sign() != 1 {
-	//		continue
-	//	}
-	//
-	//	// Check that the contract is a registered token pair
-	//	contractAddr := log.Address
-	//	id := k.GetERC20Map(ctx, contractAddr)
-	//	if len(id) == 0 {
-	//		continue
-	//	}
-	//
-	//	pair, found := k.GetTokenPair(ctx, id)
-	//	if !found {
-	//		continue
-	//	}
-	//
-	//	// Check if tokens are sent to module address
-	//	to := common.BytesToAddress(log.Topics[2].Bytes())
-	//	if !bytes.Equal(to.Bytes(), types.ModuleAddress.Bytes()) {
-	//		continue
-	//	}
-	//
-	//	// Check that conversion for the pair is enabled. Fail
-	//	if !pair.Enabled {
-	//		// continue to allow transfers for the ERC20 in case the token pair is
-	//		// disabled
-	//		k.Logger(ctx).Debug(
-	//			"ERC20 token -> Cosmos coin conversion is disabled for pair",
-	//			"coin", pair.Denom, "contract", pair.Erc20Address,
-	//		)
-	//		continue
-	//	}
-	//
-	//	// create the corresponding sdk.Coin that is paired with ERC20
-	//	coins := sdk.Coins{{Denom: pair.Denom, Amount: sdk.NewIntFromBigInt(tokens)}}
-	//
-	//	// Perform token conversion. We can now assume that the sender of a
-	//	// registered token wants to mint a Cosmos coin.
-	//	switch pair.ContractOwner {
-	//	case types.OWNER_MODULE:
-	//		_, err = k.CallEVM(ctx, erc20, types.ModuleAddress, contractAddr, true, "burn", tokens)
-	//	case types.OWNER_EXTERNAL:
-	//		err = k.bankKeeper.MintCoins(ctx, types.ModuleName, coins)
-	//	default:
-	//		err = types.ErrUndefinedOwner
-	//	}
-	//
-	//	if err != nil {
-	//		k.Logger(ctx).Debug(
-	//			"failed to process EVM hook for ER20 -> coin conversion",
-	//			"coin", pair.Denom, "contract", pair.Erc20Address, "error", err.Error(),
-	//		)
-	//		continue
-	//	}
-	//
-	//	// Only need last 20 bytes from log.topics
-	//	from := common.BytesToAddress(log.Topics[1].Bytes())
-	//	recipient := sdk.AccAddress(from.Bytes())
-	//
-	//	// transfer the tokens from ModuleAccount to sender address
-	//	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, recipient, coins); err != nil {
-	//		k.Logger(ctx).Debug(
-	//			"failed to process EVM hook for ER20 -> coin conversion",
-	//			"tx-hash", receipt.TxHash.Hex(), "log-idx", i,
-	//			"coin", pair.Denom, "contract", pair.Erc20Address, "error", err.Error(),
-	//		)
-	//		continue
-	//	}
-	//}
+	return nil
+}
+
+// UpdateCoinFromEvent update reserve and volume by event
+func (k *Keeper) UpdateCoinFromEvent(ctx sdk.Context, dataUpdate contracts.TokenReserveUpdated, tokenAddress string) error {
+
+	// Ensure coin does not exist
+	coinExist, err := k.GetCoinByDRC(ctx, tokenAddress)
+	if err != nil {
+		return nil
+	}
+
+	_ = k.UpdateCoinVR(ctx, coinExist.Denom, math.NewIntFromBigInt(dataUpdate.NewSupply), math.NewIntFromBigInt(dataUpdate.NewReserve))
+
+	// Emit transaction events
+	_ = events.EmitTypedEvent(ctx, &types.EventUpdateCoinVR{
+		Denom:   coinExist.Denom,
+		Volume:  math.NewIntFromBigInt(dataUpdate.NewSupply).String(),
+		Reserve: math.NewIntFromBigInt(dataUpdate.NewReserve).String(),
+	})
 
 	return nil
 }
 
 // CreateCoinEvent returns the coin if exists in KVStore.
 func (k *Keeper) CreateCoinEvent(ctx sdk.Context, reserve *big.Int, token contracts.DecimalTokenCenterToken, tokenAddress string) error {
-	k.Logger(ctx).Info("emitted event", token)
 
 	coinDenom := token.Symbol
-
-	var coin = types.Coin{}
 
 	// Ensure coin does not exist
 	coinExist, err := k.GetCoin(ctx, coinDenom)
@@ -233,7 +150,7 @@ func (k *Keeper) CreateCoinEvent(ctx sdk.Context, reserve *big.Int, token contra
 	authAddr := authtypes.NewModuleAddress(cointypes.ModuleName)
 
 	// Create new coin instance
-	coin = types.Coin{
+	var coin = types.Coin{
 		Title:         token.Name,
 		Denom:         coinDenom,
 		CRR:           uint32(token.Crr),
