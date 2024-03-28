bbcb9791d1bb6be8d6bde7c4f6a0ce2f59f2d454
hakerkiler <vinatorul2@gmail.com>
2024-03-29 00:21:28 +0300

    [update] add query for end queue evm contract

--- contracts/utils.go
+++ contracts/utils.go
@@ -19,6 +19,6 @@ func GetContractCenter(chainID string) string {
 	} else if helpers.IsTestnet(chainID) {
 		return "0x464eB51b5965f4520B7180E2cC7805c55f9cefDA"
 	} else {
-		return "0xbE8f533E4A894a8ef0E467296B3B76d309aeF3c8"
+		return "0xbF7BB2C9466D88D9224e9A4380287a22D2f2d3ac"
 	}
 }
