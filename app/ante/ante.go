c18b3ac4cee72d7d19d8cc2a6aea2c9d899d13e6
hakerkiler <vinatorul2@gmail.com>
2024-05-06 09:50:57 +0300

    [update] turn of cosmos type

--- app/ante/ante.go
+++ app/ante/ante.go
@@ -3,10 +3,6 @@ package ante
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
-	authante "github.com/cosmos/cosmos-sdk/x/auth/ante"
-
-	ethante "github.com/decimalteam/ethermint/app/ante"
 )
 
 // NewAnteHandler returns an ante handler responsible for attempting to route an
@@ -17,31 +13,31 @@ func NewAnteHandler(options HandlerOptions) sdk.AnteHandler {
 	return func(
 		ctx sdk.Context, tx sdk.Tx, sim bool,
 	) (newCtx sdk.Context, err error) {
-		var anteHandler sdk.AnteHandler
-
-		defer ethante.Recover(ctx.Logger(), &err)
-
-		txWithExtensions, ok := tx.(authante.HasExtensionOptionsTx)
-		if ok {
-			opts := txWithExtensions.GetExtensionOptions()
-			if len(opts) > 0 {
-				switch typeURL := opts[0].GetTypeUrl(); typeURL {
-				case "/ethermint.evm.v1.ExtensionOptionsEthereumTx":
-					// handle as *evmtypes.MsgEthereumTx
-					anteHandler = newEthAnteHandler(options)
-				//case "/ethermint.types.v1.ExtensionOptionsWeb3Tx":
-				//	// handle as normal Decimal SDK tx, except signature is checked for EIP712 representation
-				//	anteHandler = newCosmosAnteHandlerEip712(options)
-				//case "/ethermint.types.v1.ExtensionOptionDynamicFeeTx":
-				//	// cosmos-sdk tx with dynamic fee extension
-				//	anteHandler = newCosmosAnteHandler(options)
-				default:
-					return ctx, sdkerrors.ErrUnknownExtensionOptions
-				}
-
-				return anteHandler(ctx, tx, sim)
-			}
-		}
+		//var anteHandler sdk.AnteHandler
+		//
+		//defer ethante.Recover(ctx.Logger(), &err)
+		//
+		//txWithExtensions, ok := tx.(authante.HasExtensionOptionsTx)
+		//if ok {
+		//	opts := txWithExtensions.GetExtensionOptions()
+		//	if len(opts) > 0 {
+		//		switch typeURL := opts[0].GetTypeUrl(); typeURL {
+		//		case "/ethermint.evm.v1.ExtensionOptionsEthereumTx":
+		//			// handle as *evmtypes.MsgEthereumTx
+		//			anteHandler = newEthAnteHandler(options)
+		//		//case "/ethermint.types.v1.ExtensionOptionsWeb3Tx":
+		//		//	// handle as normal Decimal SDK tx, except signature is checked for EIP712 representation
+		//		//	anteHandler = newCosmosAnteHandlerEip712(options)
+		//		//case "/ethermint.types.v1.ExtensionOptionDynamicFeeTx":
+		//		//	// cosmos-sdk tx with dynamic fee extension
+		//		//	anteHandler = newCosmosAnteHandler(options)
+		//		default:
+		//			return ctx, sdkerrors.ErrUnknownExtensionOptions
+		//		}
+		//
+		//		return anteHandler(ctx, tx, sim)
+		//	}
+		//}
 
 		return ctx, sdkerrors.ErrUnknownRequest
 
