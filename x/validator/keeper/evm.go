c0ccc3ae2be84da7a84a9449c16bccdd1eae4ab3
hakerkiler <vinatorul2@gmail.com>
2024-05-21 13:59:58 +0300

    [update] update token center

--- x/validator/keeper/evm.go
+++ x/validator/keeper/evm.go
@@ -24,7 +24,9 @@ func (k *Keeper) QueryAddressDelegation(
 		return new(common.Address).Hex(), err
 	}
 	data, err := contractCenter.Unpack(methodCall, res.Ret)
-
+	if len(data) == 0 {
+		return new(common.Address).Hex(), err
+	}
 	return data[0].(common.Address).String(), err
 }
 
