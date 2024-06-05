ec3cb3490a9293aae37beb020d9e065d1d0a8758
hakerkiler <vinatorul2@gmail.com>
2024-06-05 19:48:20 +0300

    [update] add nft from evm

--- contracts/utils.go
+++ contracts/utils.go
@@ -74,6 +74,6 @@ func GetContractCenter(chainID string) string {
 	} else if helpers.IsTestnet(chainID) {
 		return "0x464eB51b5965f4520B7180E2cC7805c55f9cefDA"
 	} else {
-		return "0xa052da26a526e251db6390834009464ab0398ddc"
+		return "0x0e877d02eac44a0d4c2a33c70d44591e2081397e"
 	}
 }
