936858cd0bf670a9e57ab57671f13106795c8d3f
hakerkiler <vinatorul2@gmail.com>
2024-06-06 14:01:42 +0300

    [update] add nft from evm

--- x/validator/keeper/val_state_change.go
+++ x/validator/keeper/val_state_change.go
@@ -121,6 +121,7 @@ func (k Keeper) BlockValidatorUpdates(ctx sdk.Context) []abci.ValidatorUpdate {
 
 	//dataAddress, err := k.QueryAddressDelegation(ctx, common.HexToAddress(contracts.GetContractCenter(ctx.ChainID())))
 	dataAddress, err := contracts.GetAddressFromContractCenter(ctx, k.evmKeeper, contracts.NameOfSlugForGetAddressDelegation)
+	fmt.Println(err)
 	ifNeedExec, err := k.QueryIfNeedExecuteFinish(ctx, common.HexToAddress(dataAddress))
 	if ifNeedExec {
 		resultExec, errexec := k.ExecuteQueueEVMAction(ctx, common.HexToAddress(dataAddress))
