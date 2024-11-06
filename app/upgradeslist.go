22ed9c519fc6fe8366a93437badf7c635f370085
Andrei K. <kozak@nksupport.com>
2024-11-06 19:57:22 +0100

    Update block

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -25,6 +25,7 @@ var UpgradeListMainnet = []UpgradeCreator{
 	{"https://repo.decimalchain.com/22372801", DummyUpgradeHandlerCreator},
 	{"https://repo.decimalchain.com/22466601", ValidatorDuplicatesHandlerCreator},
 	{"https://repo.decimalchain.com/23003701", DummyUpgradeHandlerCreator},
+	{"https://repo.decimalchain.com/23116601", DummyUpgradeHandlerCreator},
 }
 
 func GetUpgradeList(chainID string) []UpgradeCreator {
