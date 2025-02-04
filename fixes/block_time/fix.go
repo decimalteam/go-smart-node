c67d590fbd97b6577a634f8ed47a0c8c69fb316b
dc <info@dc.org>
2025-02-04 13:19:36 +0300

    adjust fixed time

--- fixes/block_time/fix.go
+++ fixes/block_time/fix.go
@@ -27,7 +27,11 @@ func init() {
 }
 
 func GetFixedBlockTime(ctx sdk.Context) int64 {
-	var fixedTime = ctx.BlockTime().Unix()
+	var fixedTime = ctx.BlockTime().Unix() + 5
+	if ctx.BlockTime().Nanosecond() > 500000000 {
+		fixedTime += 1
+	}
+
 	if fix, ok := fixes[ctx.BlockHeight()]; ok {
 		fixedTime = fix
 	}
