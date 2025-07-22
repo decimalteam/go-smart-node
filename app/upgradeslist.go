474e4a54d22c0d24e198a463991d2d63104215d0
dc <info@dc.org>
2025-07-22 11:01:15 +0300

    set upgrade block

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -17,6 +17,7 @@ var UpgradeListTestnet = []UpgradeCreator{
 	{"https://testnet-repo.decimalchain.com/10328801", DummyUpgradeHandlerCreator},
 	{"https://testnet-repo.decimalchain.com/10337801", MigrationUpgradeHandlerCreator},
 	{"https://testnet-repo.decimalchain.com/15069701", DummyUpgradeHandlerCreator},
+	{"https://testnet-repo.decimalchain.com/15586201", DummyUpgradeHandlerCreator},
 }
 var UpgradeListMainnet = []UpgradeCreator{
 	{"https://repo.decimalchain.com/12830301", FixSendUpgradeHandlerCreator},
