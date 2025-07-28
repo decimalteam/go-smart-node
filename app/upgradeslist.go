71fc32d210e62dea4004c559470362ff404efe56
IFalimendikov <golovanjot2@gmail.com>
2025-07-28 12:58:21 +0200

    set upgrade block

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -30,6 +30,7 @@ var UpgradeListMainnet = []UpgradeCreator{
 	{"https://repo.decimalchain.com/23116601", DummyUpgradeHandlerCreator},
 	{"https://repo.decimalchain.com/24537501", DummyUpgradeHandlerCreator},
 	{"https://repo.decimalchain.com/25812301", DummyUpgradeHandlerCreator},
+	{"https://repo.decimalchain.com/27239601", DummyUpgradeHandlerCreator},
 }
 
 func GetUpgradeList(chainID string) []UpgradeCreator {
