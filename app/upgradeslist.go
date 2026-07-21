f88f7e41c175b2585f28de8974fd3c538c9a16ff
IFalimendikov <golovanjot2@gmail.com>
2026-07-21 12:33:41 +0200

    rebuild

--- app/upgradeslist.go
+++ app/upgradeslist.go
@@ -13,7 +13,7 @@ var UpgradeListDevnet = []UpgradeCreator{
 }
 // TODO(rewardPerBlock): at deploy, add the following entry once the contract upgrade height
 // is known (must be at or after the master-validator contract upgrade that introduces the
-// rewardPerBlock field/getter). The EVM hook keeps node state in sync afterwards; this
+// rewardPerBlock field/getter). The EVM hook keeps node state in sync afterwards this
 // one-time handler seeds any value already set on the contract:
 //   UpgradeListTestnet: {"https://testnet-repo.decimalchain.com/<HEIGHT>", RewardPerBlockSyncHandlerCreator},
 //   UpgradeListMainnet: {"https://repo.decimalchain.com/<HEIGHT>", RewardPerBlockSyncHandlerCreator},
